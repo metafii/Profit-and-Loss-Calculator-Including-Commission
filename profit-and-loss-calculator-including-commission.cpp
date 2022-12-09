b25c910d6713f07383b20b2d1f78ff6cd4bd10bb
metafii <116486878+metafii@users.noreply.github.com>
2022-12-09 19:46:07 +0300

    Add files via upload

---
+++ profit-and-loss-calculator-including-commission.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstring>
+#include <clocale>
+#include <cfloat> 
+#include <string>
+
+int main()
+{
+	float purchase = 0.00, sales = 0.00, money_earned = 0.00;
+	const float COMMISSION = 0.015;
+	std::string company_name;
+
+	std::cout << "Please write the name of the company whose share you bought, the purchase price of the share and" << std::endl << "the price at which you sold the share, with spaces in between (Commission: 0.15%):";
+
+	std::cin >> company_name >> purchase >> sales;
+
+	if (sales > purchase)
+
+	{
+		money_earned = (sales - purchase) - (sales - purchase) * COMMISSION;
+
+		std::cout << "You have a profit of $" << money_earned << " for the " << company_name << " share.";
+
+		return 0;
+	}
+
+
+	else if (sales < purchase)
+
+	{
+		money_earned = (purchase - sales) - (purchase - sales) * COMMISSION;
+
+		std::cout << "You have a loss of $" << money_earned << " for the " << company_name << " share.";
+
+		return 0;
+	}
+
+	else (sales = purchase);
+	{
+		money_earned = (purchase - sales) - (purchase - sales) * COMMISSION;
+
+		std::cout << company_name << " shares have no profit or loss status.";
+
+		return 0;
+	}
+}
